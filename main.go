c03af914c79d0805d9e293005d63ce1732a290b5
Aleksei Kashintsev <aleksei_kashintsev@epam.com>
2018-08-02 18:37:23 +0300

    123

--- main.go
+++ main.go
@@ -20,13 +20,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
-	"gopkg.in/ini.v1"
+	ini "gopkg.in/ini.v1"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	types "k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -34,55 +41,70 @@ import (
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
-type Element struct {
-	Key   string
-	Value string
+// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
+// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+
+type endpoints map[int]string
+
+type realServers struct {
+	address string
+	port    int
+	weight  int
 }
 
-type Conf struct {
-	element []Element
+type virtual struct {
+	hostname  string
+	port      int
+	protocol  string
+	scheduler string
+	real      map[int]realServers
 }
 
-var CONNECTORS int = 16
+type virtuals map[string]virtual
 
-var Confmap map[string]Conf
-var configurationFile = "daemon.ini"
-var cfg = ini.Empty()
+var globalConfig = make(virtuals, 0)
 
-func processAddToConfiguration(applicationServerHostname string, applicationServerIP string, applicationServerPort int) error {
+type service struct {
+	label           string
+	namespace       string
+	destinationPort int
+}
 
-	for i := 1; i <= CONNECTORS; i++ {
-		var lvsname = fmt.Sprintf("%s%d", "lvs", i)
-		cfg.Section(lvsname).Key(applicationServerHostname).SetValue(fmt.Sprintf("%s:%d", applicationServerIP, applicationServerPort))
+type configFile struct {
+	name string
+}
+
+type configuration struct {
+	service    service
+	configFile configFile
+	clientSet  *kubernetes.Clientset
+}
+
+var cnf configuration
+var vReg = regexp.MustCompile(`(?m)^virtual = (\S*)$`)
+var subVirtualReg = regexp.MustCompile(`(?m)^(\S*):(\d+)$`)
+var protocolReg = regexp.MustCompile(`(?m)^\s*protocol = (\S*)$`)
+var schedulerReg = regexp.MustCompile(`(?m)^\s*scheduler = (\S*)$`)
+var realReg = regexp.MustCompile(`(?m)^\s*real = (\S*):(\d+) gate (\d+)$`)
+var emptyReg = regexp.MustCompile(`(?m)^$`)
+
+//setup homeDir
+func HomeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
 	}
-
-	cfg.SaveTo(configurationFile)
-	return nil
+	return os.Getenv("USERPROFILE") // windows
 }
 
-func processVerifyConfiguration(applicationServerHostname string, applicationServerIP string, applicationServerPort int) error {
-	for i := 1; i <= CONNECTORS; i++ {
-		var lvsname = fmt.Sprintf("%s%d", "lvs", i)
-		cfg.Section(lvsname).DeleteKey(applicationServerIP)
+func check(e error) {
+	if e != nil {
+		log.Fatal(e.Error())
 	}
-
-	cfg.SaveTo(configurationFile)
-	return nil
 }
 
-func processDeleteFromConfiguration(applicationServerHostname string) error {
-	for i := 1; i <= CONNECTORS; i++ {
-		var lvsname = fmt.Sprintf("%s%d", "lvs", i)
-		cfg.Section(lvsname).DeleteKey(applicationServerHostname)
-	}
-
-	cfg.SaveTo(configurationFile)
-	return nil
-}
-
-func main() {
+func initK8S() (*kubernetes.Clientset, error) {
 	var kubeconfig *string
-	if home := homeDir(); home != "" {
+	if home := HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
@@ -92,37 +114,236 @@ func main() {
 	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 
 	// create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
+	return clientset, nil
+}
 
-	// Examples for error handling:
-	// - Use helper functions like e.g. errors.IsNotFound()
-	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-	namespace := "default"
-	svc := "test-nginx"
-	var filter = fmt.Sprintf("k8s-app=%s", svc)
-
-	//verify configurationFile
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{LabelSelector: filter})
+func initNow() {
+	cfg, err := ini.Load("config.ini")
 	if err != nil {
-		panic(err.Error())
+		check(err)
 	}
-	fmt.Printf("There are %d pods in the cluster\n Syncronising configuration...\n", len(pods.Items))
+	cnf.configFile.name = cfg.Section("main").Key("lvsConfigFilePath").String()
 
-	for _, val := range pods.Items {
-		processAddToConfiguration(val.Name, val.Status.PodIP, 8080)
-	}
+	cnf.service.label = cfg.Section("main").Key("labelToMonitorName").String()
+	cnf.service.namespace = cfg.Section("main").Key("labelToMonitorNamespace").String()
+	cnf.service.destinationPort, _ = cfg.Section("main").Key("lvsDestionationPort").Int()
 
-	watcher, err := clientset.Core().Pods(namespace).Watch(metav1.ListOptions{LabelSelector: filter})
+	cnf.clientSet, err = initK8S()
+	check(err)
+}
+
+func readConfig(path string) ([]string, error) {
+	content, err := ioutil.ReadFile(path)
 	if err != nil {
-		fmt.Printf("Nothing found with k8s-app=test-nginx")
+		return nil, err
 	}
+
+	data := strings.Split(string(content), "\n")
+	return data, nil
+}
+
+func readWholeIni(path string) {
+	data, err := readConfig(path)
+	check(err)
+
+	var realAddressPosition = 0
+	//initialize first element
+	var vt virtual
+	vt.real = make(map[int]realServers)
+	var currentVirtualServer string
+
+	for i := 0; i < len(data); i++ {
+		if vReg.MatchString(data[i]) {
+			s := vReg.FindStringSubmatch(data[i])
+			realAddressPosition = 0
+			currentVirtualServer = s[1]
+			z := subVirtualReg.FindStringSubmatch(s[1])
+			vt.hostname = z[1]
+			p, _ := strconv.Atoi(z[2])
+			vt.port = p
+		}
+
+		if protocolReg.MatchString(data[i]) {
+			s := protocolReg.FindStringSubmatch(data[i])
+			vt.protocol = s[1]
+		}
+
+		if schedulerReg.MatchString(data[i]) {
+			s := schedulerReg.FindStringSubmatch(data[i])
+			vt.scheduler = s[1]
+		}
+
+		if realReg.MatchString(data[i]) {
+			s := realReg.FindStringSubmatch(data[i])
+			var srv realServers
+			srv.address = s[1]
+			if !checkSpecifiedPodExists(cnf.clientSet, srv.address, cnf.service.namespace) {
+				continue
+			}
+
+			port, _ := strconv.Atoi(s[2])
+			srv.port = port
+			weight, _ := strconv.Atoi(s[3])
+			srv.weight = weight
+
+			vt.real[realAddressPosition] = srv
+			realAddressPosition++
+		}
+
+		if emptyReg.MatchString(data[i]) {
+			globalConfig[currentVirtualServer] = vt
+			vt.real = make(map[int]realServers)
+		}
+
+	}
+}
+
+func updateWight(lvs string, srv realServers) {
+	realData := globalConfig[lvs].real
+	for i := 0; i < len(realData); i++ {
+		if realData[i].address == srv.address && realData[i].port == srv.port {
+			globalConfig[lvs].real[i] = srv
+			return
+		}
+	}
+	globalConfig[lvs].real[len(realData)] = srv
+	return
+}
+
+func updateWeighAllLVS(srv realServers) {
+	for k := range globalConfig {
+		updateWight(k, srv)
+	}
+}
+
+func writeEach(virtualAddress string, v virtual, f *os.File) {
+	_, _ = f.WriteString(fmt.Sprintf("real = %s\n", virtualAddress))
+	_, _ = f.WriteString(fmt.Sprintf("     protocol = %s\n", v.protocol))
+	_, _ = f.WriteString(fmt.Sprintf("     scheduler = %s\n", v.scheduler))
+	for i := 0; i < len(v.real); i++ {
+		if v.real[i].address != "" {
+			_, _ = f.WriteString(fmt.Sprintf("     real = %s:%d gate %d\n", v.real[i].address, v.real[i].port, v.real[i].weight))
+		}
+	}
+	_, _ = f.WriteString("\n")
+}
+
+//TODO: make this function stable
+//because range is chaos order
+func writeConfig(path string) {
+	f, err := os.Create(path)
+	check(err)
+	defer f.Close()
+
+	for virtualHost, v := range globalConfig {
+		writeEach(virtualHost, v, f)
+	}
+
+	f.Sync()
+}
+
+func checkSpecifiedPodExists(set *kubernetes.Clientset, podName string, namespace string) bool {
+	_, err := set.CoreV1().Pods(namespace).Get(podName, metav1.GetOptions{})
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+type statesType map[string]bool
+
+var states = make(statesType)
+
+func addNewConnectorToLVSGracefully(pod v1.Pod, progress int) {
+	if states[pod.Name] == true {
+		return
+	}
+
+	i := progress
+	for {
+		i += 20
+		if i > 100 {
+			i = 100
+		}
+		if states[pod.Name] == false {
+			states[pod.Name] = true
+		}
+
+		if !checkSpecifiedPodExists(cnf.clientSet, pod.Name, pod.Namespace) {
+			states[pod.Name] = false
+			return
+		}
+
+		progress := strconv.Itoa(i)
+		data := []byte(`{"metadata": {"labels": {"progress": "` + progress + `"}}}}`)
+		_, err := cnf.clientSet.CoreV1().Pods(cnf.service.namespace).Patch(pod.Name, types.StrategicMergePatchType, data, "")
+		check(err)
+		var srv realServers
+		srv.address = pod.Name
+		srv.port = cnf.service.destinationPort
+		srv.weight = i
+		updateWeighAllLVS(srv)
+		writeConfig(cnf.configFile.name)
+
+		if i == 100 {
+			delete(states, pod.Name)
+			break
+		} else {
+			time.Sleep(15 * time.Second)
+		}
+	}
+
+}
+
+func deleteRealServer(lvs string, hostname string) {
+	data := globalConfig[lvs].real
+	for i := 0; i < len(data); i++ {
+		if data[i].address == hostname {
+			delete(globalConfig[lvs].real, i)
+			writeConfig(cnf.configFile.name)
+		}
+	}
+}
+
+func deleteRealServerAllLVS(hostname string) {
+	for k := range globalConfig {
+		deleteRealServer(k, hostname)
+	}
+}
+
+func loadActualStateOfInfrastructure() {
+	pods, err := cnf.clientSet.CoreV1().Pods(cnf.service.namespace).List(metav1.ListOptions{LabelSelector: cnf.service.label})
+	check(err)
+	for _, pod := range pods.Items {
+		var srv realServers
+		srv.address = pod.Name
+		srv.port = cnf.service.destinationPort
+		weight, _ := strconv.Atoi(pod.Labels["progress"])
+		srv.weight = weight
+		updateWeighAllLVS(srv)
+	}
+	writeConfig(cnf.configFile.name)
+}
+
+func detectNewConnectors(c configuration) {
+	pods, err := c.clientSet.CoreV1().Pods(c.service.namespace).List(metav1.ListOptions{LabelSelector: c.service.label})
+	check(err)
+	log.Printf("There are %d pods now in the cluster with this label: %s\n", len(pods.Items), c.service.label)
+	if len(pods.Items) == 0 {
+		log.Fatalf("No running pods with label: %s", c.service.label)
+		log.Fatal("Please ensure what you have at least one pod up and running")
+	}
+
+	watcher, err := c.clientSet.CoreV1().Pods(c.service.namespace).Watch(metav1.ListOptions{LabelSelector: c.service.label})
+	check(err)
 
 	ch := watcher.ResultChan()
 
@@ -132,26 +353,46 @@ func main() {
 			log.Fatal("unexpected type")
 		}
 
-		fmt.Printf("EVENT: %s Name: %s IP: %s PHASE: %s\n", event.Type, pod.Name, pod.Status.PodIP, pod.Status.Phase)
+		log.Println("Detect new pod: ", pod.Name)
+		progress, err := strconv.Atoi(pod.Labels["progress"])
+		check(err)
 
 		switch event.Type {
+
 		case watch.Added:
 			if pod.Status.Phase == "Running" {
-				processAddToConfiguration(pod.Name, pod.Status.PodIP, 8080)
+				if progress != 100 {
+					go addNewConnectorToLVSGracefully(*pod, progress)
+				}
 			}
 		case watch.Modified:
 			if pod.Status.Phase == "Running" {
-				processAddToConfiguration(pod.Name, pod.Status.PodIP, 8080)
+				if progress != 100 {
+					go addNewConnectorToLVSGracefully(*pod, progress)
+				}
 			}
 		case watch.Deleted:
-			processDeleteFromConfiguration(pod.Name)
+			deleteRealServerAllLVS(pod.Name)
+			log.Printf("Connector deleted: %s\n", pod.Name)
+			log.Printf("Please avoid this! This is unexpected to you TCP clients")
 		}
 	}
 }
 
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
+func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	initNow()
+	readWholeIni(cnf.configFile.name)
+	loadActualStateOfInfrastructure()
+	go detectNewConnectors(cnf)
+
+	for {
+		for _ = range c {
+			log.Println("Ctrl+C pressed, exiting...")
+			os.Exit(0)
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
-	return os.Getenv("USERPROFILE") // windows
 }
